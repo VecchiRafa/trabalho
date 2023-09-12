2ce0e395dbd1faaaff3db3e10cf5880d0cdcb551
VecchiRafa <rafaelapelentier@gmail.com>
2023-09-12 15:29:36 -0300

    NÃO

--- aunimal_hotel/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.base import Base
-from models.pessoa import Pessoa
-from models.cliente_pessoa import ClientePessoa
--- aunimal_hotel/utils/database_utils.py
+++
@@ -1,5 +0,0 @@
-from services.database import engine
-from models import *
-
-def create_db():
-    Base.metadata.create_all(bind=engine) 
