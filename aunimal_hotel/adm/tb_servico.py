19f97d82fe62678ef2cb77c161958f7c6c3e83a2
VecchiRafa <rafaelapelentier@gmail.com>
2023-09-12 21:31:05 -0300

    s

--- aunimal_hotel/adm/tb_servico.py
+++ aunimal_hotel/adm/tb_servico.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 # Configuração da conexão com o banco de dados usando SQLAlchemy
-engine = create_engine('mysql://root:Brother25525&@localhost/aunimal_hotel')
+engine = create_engine('mysql://root:Brother25525&@localhost/aunimal_hotel_teste')
 Session = sessionmaker(bind=engine)
 
 # Crie uma instância da classe Base
@@ -13,56 +13,65 @@ Base = declarative_base()
 class Servico(Base):
     __tablename__ = "servico"
     
-    id_servico = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(200), nullable=False)
-    preco = Column(Float, nullable=False)
-    duracao_estimada = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    valor_total = Column(Float, nullable=False)
     descricao = Column(Text, nullable=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    
 
 def listar_servicos(session):
-    # Consultar serviços
-    servicos = session.query(Servico).all()
+    # Consultar serviços com as informações desejadas (id, valor_total e descricao)
+    servicos = session.query(Servico.id, Servico.valor_total, Servico.descricao).all()
+    
     for servico in servicos:
-        print(f"ID: {servico.id_servico}, Nome: {servico.nome}, Preço: {servico.preco}, Duração Estimada: {servico.duracao_estimada}, Descrição: {servico.descricao}")
+        id, valor_total, descricao = servico
+        print(f"id: {id}, Valor Total: {valor_total}, Descrição: {descricao}")
 
 def adicionar_servico(session):
     # Coletar informações do usuário
-    nome = input("Digite o nome do serviço: ")
-    preco = float(input("Digite o preço do serviço: "))
-    duracao_estimada = int(input("Digite a duração estimada do serviço (em minutos): "))
+    valor_total = float(input("Digite o valor total do serviço: "))  # Corrigimos para 'valor_total'
     descricao = input("Digite a descrição do serviço (opcional): ")
     
     # Criar uma nova instância de Servico
-    novo_servico = Servico(nome=nome, preco=preco, duracao_estimada=duracao_estimada, descricao=descricao)
+    novo_servico = Servico(valor_total=valor_total, descricao=descricao)  # Corrigimos para 'valor_total'
     
     # Adicionar o serviço à sessão
     session.add(novo_servico)
     
-    # Commit para salvar no banco de dados
-    session.commit()
-    print("Serviço adicionado com sucesso!")
+    try:
+        # Commit para salvar no banco de dados
+        session.commit()
+        print("Serviço adicionado com sucesso!")
+    except Exception as e:
+        # Em caso de erro, faça o rollback e mostre a mensagem de erro
+        session.rollback()
+        print(f"Erro ao adicionar o serviço: {e}")
 
-# Iniciar uma sessão
-session = Session()
 
-while True:
-    print("\nOpções:")
-    print("1. Listar serviços")
-    print("2. Adicionar serviço")
-    print("3. Sair")
-    
-    escolha = input("Escolha uma opção: ")
-    
-    if escolha == "1":
-        listar_servicos(session)
-    elif escolha == "2":
-        adicionar_servico(session)
-    elif escolha == "3":
-        print("Encerrando o programa.")
-        break
-    else:
-        print("Opção inválida. Tente novamente.")
 
-# Fechar a sessão quando terminar
-session.close()
+def executar():
+    # Iniciar uma sessão
+    session = Session()
+
+    while True:
+        print("\nOpções:")
+        print("1. Listar serviços")
+        print("2. Adicionar serviço")
+        print("3. Sair")
+        
+        escolha = input("Escolha uma opção: ")
+        
+        if escolha == "1":
+            listar_servicos(session)
+        elif escolha == "2":
+            adicionar_servico(session)
+        elif escolha == "3":
+            print("Encerrando o programa.")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+    # Fechar a sessão quando terminar
+    session.close()
+
+if __name__ == "__main__":
+    executar()
