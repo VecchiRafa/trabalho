fd2fcfaca9ad265b8baa5410715ffc22164725c9
VecchiRafa <rafaelapelentier@gmail.com>
2023-09-13 16:14:31 -0300

    mudo

--- aunimal/main.py
+++
@@ -1,7 +0,0 @@
-from services.database import session
-from models import ClientePessoa
-from utils.database_utils import create_db
-
-if __name__ == "__main__":
-    print("Criando o Banco de Dados!!")
-    create_db()
--- aunimal/models/funcionario_pessoa.py
+++
@@ -1,6 +0,0 @@
-from models import Base, Pessoa
-from sqlalchemy import VARCHAR, ForeignKey, TIMESTAMP, DECIMAL
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.dialects.mysql import MEDIUMINT
-from datetime import datetime, date
-
