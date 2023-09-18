294056b16ed127509fafd4d6d28b88ec55261df5
VecchiRafa <rafaelapelentier@gmail.com>
2023-09-17 22:56:51 -0300

    TE AMO LOCAMENTE E INSANAMENTE MEU AMOR, I LOVE U

--- TRABALHO1/loja.c
+++ TRABALHO1/loja.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <locale.h>
 
+const float Chocolate = 108; 
+const float Marshmallow = 10.50;
+const float Brownie = 30.00;
+const float Ice_cream = 15.00;
+const float Cake = 17.50;
+const float Donut = 12.90;
+const float Cookie = 20.70;
+const float Candy = 31.44;
+const float Cupcake = 46.02;
+const float Pudding = 55.80;
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
@@ -14,7 +25,7 @@ int main()
     char linha_produto[1024];
     char linha_vendedor[1024];
 
-    float somaTotal = 0;
+    //float somaTotal = 0;
     //float totalProdutos = 0;
     //float totalVendedor = 0;
 
@@ -47,19 +58,63 @@ int main()
     }
 
     // arrumar
+
+
+    printf("\n\n");
+    printf("Dados das vendas:\n");
+
+    // Fechar e reabrir o arquivo de vendas para ler novamente
+    fclose(arq_venda);
+    arq_venda = fopen("vendas.txt", "r");
     
-    while (fgets(linha_produto, sizeof(linha_produto), arq_produto) && fgets(linha_venda, sizeof(linha_venda), arq_venda)) {
-        int meio, inicio, fim, meioProduto;
-        int elementosLidos1 = sscanf(linha_produto, "%d %*d %*d", &inicio); // Lê o elemento do meio da linha do arq_produto
-        int elementosLidos4 = sscanf(linha_produto, "%*d %f %*d", &meioProduto);
-        int elementosLidos2 = sscanf(linha_venda, "%*d %d %*d", &meio); // Lê o elemento do meio da linha do arq_venda
-        int elementosLidos3 = sscanf(linha_venda, "%*d %*d %d", &fim);
-        if (meio == 108) {
-            int multiplicacao = fim * 25.00;
-            printf("Resultado da multiplicação: %d\n", multiplicacao);
-        } else {
-            printf("Deu algum erro\n");
+    int contador_linhas = 0;
+
+     while (fgets(linha_venda, sizeof(linha_venda), arq_venda) != NULL) {
+        int elementosLidos, multiplicador;
+        sscanf(linha_venda, "%*d %d %d", &elementosLidos, &multiplicador);
+
+        if (elementosLidos == 108) {
+            float resultado = Chocolate * multiplicador;
+            printf("Chocolate = %.2f\n", resultado);
         }
+        if (elementosLidos == 209) {
+            float resultado = Marshmallow * multiplicador;
+            printf("Marshmallow = %.2f\n", resultado);
+        }
+        if (elementosLidos == 308) {
+            float resultado = Brownie * multiplicador;
+            printf("Brownie = %.2f\n", resultado);
+        }
+        if (elementosLidos == 407) {
+            float resultado = Ice_cream * multiplicador;
+            printf("Ice cream = %.2f\n", resultado);
+        }
+        if (elementosLidos == 506) {
+            float resultado = Cake * multiplicador;
+            printf("Cake = %.2f\n", resultado);
+        }
+        if (elementosLidos == 605) {
+            float resultado = Donut * multiplicador;
+            printf("Donut = %.2f\n", resultado);
+        }
+        if (elementosLidos == 704) {
+            float resultado = Cookie * multiplicador;
+            printf("Cookie = %.2f\n", resultado);
+        }
+        if (elementosLidos == 803) {
+            float resultado = Candy * multiplicador;
+            printf("Candy = %.2f\n", resultado);
+        }
+        if (elementosLidos == 902) {
+            float resultado = Cupcake * multiplicador;
+            printf("Cupcake = %.2f\n", resultado);
+        }
+        if (elementosLidos == 101) {
+            float resultado = Pudding * multiplicador;
+            printf("Pudding = %.2f\n",resultado);
+        }
+        contador_linhas++;
+       
     }
 
     //printf("\n\n");
