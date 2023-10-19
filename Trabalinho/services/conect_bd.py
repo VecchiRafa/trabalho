eeb1c43f490f11a7c22f965f79188b1c4a872603
VecchiRafa <rafaelapelentier@gmail.com>
2023-10-19 00:52:13 -0300

    atualizado 19/10/2023 00:52

--- Trabalinho/services/conect_bd.py
+++ Trabalinho/services/conect_bd.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Configuração da conexão com o banco de dados usando SQLAlchemy
 engine = create_engine('mysql://root:Brother25525&@localhost/aunimal_hotel_pettt')
 Session = sessionmaker(bind=engine)
