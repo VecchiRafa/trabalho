026b5ca05a7b8729e7be89df642872c059049eca
VecchiRafa <rafaelapelentier@gmail.com>
2023-10-24 20:44:51 -0300

    s

--- tentativas/Trabalinho1/models/tb_pessoa.py
+++ tentativas/Trabalinho1/models/tb_pessoa.py
@@ -2,7 +2,7 @@ from sqlalchemy import DATETIME, DATE, VARCHAR, CHAR, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.mysql import INTEGER
 from datetime import datetime, date
-from services.conect_bd import Session
+from services.conect_bd import Session, Base
 
 
 #==========================================================================================================================================================================
@@ -22,7 +22,7 @@ class Pessoa(Base):
     nacionalidade: Mapped[str] = mapped_column(VARCHAR(100), nullable=False, default='Brasil')
     data_criacao: Mapped[datetime] = mapped_column(DATETIME, nullable=False, default=datetime.now())
 
-
+    
 
 #==========================================================================================================================================================================
 # listar pessoa.
